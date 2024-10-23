3fe3b97c4ee6a9860fa6252b449bc805ab66a19e
Eleni Houvarda <houvarem@dukes.jmu.edu>
2024-10-23 13:47:16 -0400

    stubbed out methods

---
+++ HouvardaEleniMannLandon.java
@@ -0,0 +1,25 @@
+public class HouvardaEleniMannLandon {
+    // calculate decryption key from given encryption key
+    public int[][] findDecryptionKey(int encryptionKey[][]) {
+        return encryptionKey;
+
+    }
+
+    // encrypt multiple letter plaintext msg with given key
+    public int[] encrypt(int plaintext[], int encryptionKey[][]) {
+        return plaintext;
+    }
+
+    // decrypt multy letter cipher text with with given key
+    public int[] decrypt(int ciphertext[], int decryptionKey[][]) {
+        return ciphertext;
+
+    }
+
+    public static void main(String[] args) {
+        int[][] encryptionKey = { { 16, 9 }, { 7, 14 } };
+        // def plaintext (JMUCSISCOOL with letter vals)
+        // def ciphertext (MQGVGQSMJI) //not the same as one generated earlier
+        // def decryption key
+    }
+}
