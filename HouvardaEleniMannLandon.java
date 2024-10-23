b37ba24a80157b673114d54d0271cd546ab0ac55
Eleni Houvarda <houvarem@dukes.jmu.edu>
2024-10-23 14:31:20 -0400

    finddecryptionkey

--- HouvardaEleniMannLandon.java
+++ HouvardaEleniMannLandon.java
@@ -1,8 +1,17 @@
 public class HouvardaEleniMannLandon {
     // calculate decryption key from given encryption key
     public int[][] findDecryptionKey(int encryptionKey[][]) {
-        return encryptionKey;
-
+        // mult inverse of encryption key mod 26
+        int a = encryptionKey[0][0];
+        int b = encryptionKey[1][0];
+        int c = encryptionKey[0][1];
+        int d = encryptionKey[1][1];
+        int newa = d / (a * d - b * c);
+        int newb = (-b) / (a * d - b * c);
+        int newc = (-c) / (a * d - b * c);
+        int newd = a / (a * d - b * c);
+        int[][] decryptionKey = { { newa, newc }, { newb, newd } };
+        return decryptionKey;
     }
 
     // encrypt multiple letter plaintext msg with given key
@@ -18,6 +27,8 @@ public class HouvardaEleniMannLandon {
 
     public static void main(String[] args) {
         int[][] encryptionKey = { { 16, 9 }, { 7, 14 } };
+        // a16 b7
+        // c9 d14
         // def plaintext (JMUCSISCOOL with letter vals)
         // def ciphertext (MQGVGQSMJI) //not the same as one generated earlier
         // def decryption key
