fcbad21ff821d90d6f442debb787e419d2769ca8
Eleni Houvarda <houvarem@dukes.jmu.edu>
2024-10-23 20:31:35 -0400

    decryptionkey and encrypt works

--- HouvardaEleniMannLandon.java
+++ HouvardaEleniMannLandon.java
@@ -28,7 +28,7 @@ public class HouvardaEleniMannLandon {
     public static int[] encrypt(int plaintext[], int encryptionKey[][]) {
         int[] encryptedText = new int[plaintext.length];
         // for each pair
-        for (int i = 0; i < plaintext.length; i++) {
+        for (int i = 0; i < plaintext.length - 1; i++) {
             encryptedText[i] = (encryptionKey[0][0] * plaintext[i]
                     + encryptionKey[0][1] * plaintext[i + 1]) % 26;
 
@@ -45,18 +45,28 @@ public class HouvardaEleniMannLandon {
     }
 
     public static void main(String[] args) {
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         int[][] encryptionKey = { { 16, 9 }, { 7, 14 } };
         int[][] decryptionKey = findDecryptionKey(encryptionKey);
+        System.out.println("DECRYPTION KEY");
+
         System.out.println(decryptionKey[0][0] + " " + decryptionKey[1][0]);
         System.out.println(decryptionKey[0][1] + " " + decryptionKey[1][1]);
-        // A) PRINT DECRYPTION KEY
+        // A) PRINT DECRYPTION KEY (works :))
         // a16 b7
         // c9 d14
 
         // def plaintext (JMUCSISCOOL with letter vals) = 9 12 20 8 18 2 14 14 11
-        // int[] plaintext = { 9, 12, 20, 8, 18, 2, 14, 14, 11, 25 }; // 25 is the extra
-        // encrypt(plaintext, encryptionKey);
+        int[] plaintext = { 9, 12, 20, 8, 18, 2, 14, 14, 11, 25 }; // 25 is the extra
+        int[] encryption = encrypt(plaintext, encryptionKey);
         // B) PRINT OUT ENCRYPTION IN LETTERS NOT NUMBERS
+        System.out.println("ENCRYPTION IN LETTERS");
+        System.out.println(alphabet.charAt(encryption[0]) + " " + alphabet.charAt(encryption[1]) + " "
+                + (alphabet.charAt(encryption[2]) + " " + alphabet.charAt(encryption[3])));
+        // System.out.println("check with normal nums: " + encryption[0] + " " +
+        // encryption[1] + " " + encryption[2] + " "
+        // + encryption[3]);
+
         // def ciphertext (MQGVGQSMJI) //not the same as one generated earlier
         // decrypt
         // C) PRINOUT OUT CLEARTEXT IN LETTERS
